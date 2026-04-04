8eea7fbce6d2400c0cf705f09f740cc468e8544a
archalig <archie.a.aligsao@gmail.com>
2026-04-05 07:01:55 +0800

    Fixed all Vercel deployment issues

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
       <h4>Developed by Archie Aligsao</h4>
       <h4>Copyright &copy; 2025 archalig</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/archalig" target='_blank'><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/archiealigsao/" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:archie.a.aligsao@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://www.kaggle.com/aaachiii" target="_blank"><SiKaggle/></a>
+        <a href="https://github.com/archalig" target='_blank' rel='noreferrer'><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/archiealigsao/" target='_blank' rel='noreferrer'><FaLinkedin/></a>
+        <a href='mailTo:archie.a.aligsao@gmail.com' target='_blank' rel='noreferrer'><GrMail/></a>
+        <a href="https://www.kaggle.com/aaachiii" target="_blank" rel='noreferrer'><SiKaggle/></a>
       </div>
     </footer>
   )
